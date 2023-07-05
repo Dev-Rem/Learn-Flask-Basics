eeebbcb16f45a95ff1de7b2eb03286ab73eb3b60
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-07-05 13:32:39 +0100

    Define database schema and initialize database

--- wrytCL/__init__.py
+++ wrytCL/__init__.py
@@ -1,7 +1,8 @@
 import os
 
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from wrytCL.config import Config
 
 def create_app(test_config=None):
     # create and configure the app
@@ -25,8 +26,14 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
